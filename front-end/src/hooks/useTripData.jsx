64fa48778114af43b92ae3571cc011db3eed8c25
Esther2093 <estherlee0420@gmail.com>
2023-09-24 10:55:54 +1000

    taking out fetch hook

--- front-end/src/hooks/useTripData.jsx
+++ front-end/src/hooks/useTripData.jsx
@@ -2,27 +2,31 @@ import { useState, useEffect } from "react";
 
 function useTripData() {
   const [allTrips, setAllTrips] = useState([]);
-  const [filteredTrips, setFilteredTrips] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
+      const data = [];
+
       try {
         // Fetch trip data
-        const tripResponse = await fetch("http://localhost:8000/api/trips");
-        const tripResult = await tripResponse.json();
-        if (tripResult.data) {
-          setAllTrips(tripResult.data);
-          setFilteredTrips(tripResult.data);
+        const response = await fetch("http://localhost:8000/api/trips");
+        const result = await response.json();
+
+        if (result.data) {
+          data.push(...result.data);
+          
         }
       } catch (error) {
         console.error("Error fetching trip data:", error);
       }
+      
+      setAllTrips(data);
     };
 
     fetchData();
   }, []);
 
-  return { allTrips, filteredTrips };
+  return { allTrips };
 }
 
 export default useTripData;
